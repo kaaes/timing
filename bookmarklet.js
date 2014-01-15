cf97d287dd4239e5c0bb031ab4973b30322f4fc1
Kasia <kasia@drzyzga.pl>
2014-01-15 20:19:24 +0100

    Use profiler.js from https://raw.github.com
    This way bookmarklet should load the file even for sites
    served by https (of course only the ones that don't explicitly
    block scripts like this e.g. Facebook)

--- bookmarklet.js
+++ bookmarklet.js
@@ -13,11 +13,10 @@
 					window.__profiler.init();
 					__profiler.scriptLoaded=true;
 				},
-				t=new Date();
 		s.type='text/javascript';
 		l.style.cssText='z-index:999;position:fixed;top:10px;left:10px;display:inline;width:auto;font-size:14px;line-height:1.5em;font-family:Helvetica,Calibri,Arial,sans-serif;text-shadow:none;padding:3px 10px 0;background:#FFFDF2;box-shadow:0 0 0 3px rgba(0,0,0,.25),0 0 5px 5px rgba(0,0,0,.25); border-radius:1px';
 		l.innerHTML='Just a moment';
-		s.src='http://kaaes.github.com/timing/profiler.js?'+t.getTime();
+		s.src='https://raw.github.com/kaaes/timing/gh-pages/profiler.js';
 		s.onload=c;
 		s.onreadystatechange=function(){if(this.readyState=='loaded'){c()}};
 		d.body.appendChild(l);
